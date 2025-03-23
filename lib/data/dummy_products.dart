f1df22ae71f53e5cbc30abe09921eafec186326b
Jimoh Yusuf <yusuphjoluwaseun@gmail.com>
2025-03-23 09:36:53 +0000

    move data from data directory to the home model

--- lib/data/dummy_products.dart
+++
@@ -1,9 +0,0 @@
-import 'package:cherry_mvp/core/models/product.dart';
-import 'package:cherry_mvp/core/config/config.dart';
-
-const dummyProducts = [
-  Product(name: "Men's Grey Button-up T-Shirt", product_image: AppImages.mensTShirt, price: 7.00, charity_image: AppImages.homeStart, likes: 8, number: 8),
-  Product(name: "Men's Grey Button-up T-Shirt", product_image: AppImages.mensTShirt, price: 7.00, charity_image: AppImages.homeStart, likes: 8, number: 8),
-  Product(name: "Men's Grey Button-up T-Shirt", product_image: AppImages.mensTShirt, price: 7.00, charity_image: AppImages.homeStart, likes: 8, number: 8),
-  Product(name: "Men's Grey Button-up T-Shirt", product_image: AppImages.mensTShirt, price: 7.00, charity_image: AppImages.homeStart, likes: 8, number: 8),
-];
