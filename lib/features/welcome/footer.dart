21d8d192c676dee48c816dd6381728f41be5aa36
bonaberibonaberi1 <onoceaniesales@gmail.com>
2025-05-15 22:42:33 +0100

    update

--- lib/features/welcome/footer.dart
+++ lib/features/welcome/footer.dart
@@ -12,19 +12,19 @@ class Footer extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [   
-        SizedBox( 
-          height: 15, 
-        ), 
-
-        InkWell(
-          onTap: () {},
-          child: Container( 
-            child: Text(
-              AppStrings.delete_account_Text,
-              style: TextStyle(fontSize: 13, color: AppColors.primary, fontWeight: FontWeight.w500,),
+        Padding(
+          padding: EdgeInsets.only(top: 15),
+          child: InkWell(
+            onTap: () {},
+            child: Container( 
+              child: Text(
+                AppStrings.delete_account_Text,
+                style: TextStyle(fontSize: 13, color: AppColors.primary, fontWeight: FontWeight.w500,),
+              ),
             ),
           ),
         ),
+
 
         Container( 
           child: Text(
