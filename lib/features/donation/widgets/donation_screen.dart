4eee0043c2477340a324e2c9faebbbe811e35456
bonaberibonaberi1 <onoceaniesales@gmail.com>
2025-06-04 12:18:22 +0100

    154 update 1

--- lib/features/donation/widgets/donation_screen.dart
+++ lib/features/donation/widgets/donation_screen.dart
@@ -3,6 +3,7 @@ import 'package:cherry_mvp/core/config/config.dart';
 import 'package:cherry_mvp/features/donation/widgets/donation_options.dart'; 
 import 'package:cherry_mvp/features/donation/widgets/form_fields.dart'; 
 import 'package:cherry_mvp/features/donation/widgets/dropdown_fields.dart'; 
+import 'package:cherry_mvp/features/donation/donation_model.dart';
 
 
 class DonationScreen extends StatefulWidget {
@@ -38,30 +39,12 @@ class DonationScreenState extends State<DonationScreen> {
   } 
 
   
+  
   final TextEditingController _titleController = TextEditingController(); 
-  final String TitleHintText = 'e.g. White COS Jumper';
-  final String TitleText = AppStrings.titleText;
 
   final TextEditingController _descriptionController = TextEditingController(); 
-  final String DescriptionHintText = "e.g. only worn a few times, brand x, true to size, no label.";
-  final String DescriptionText = AppStrings.descriptionText;
 
   final TextEditingController _addToCollectionController = TextEditingController(); 
-  final String AddToCollectionHintText = "Add to Collection";
-  final String AddToCollectionText = "";
-
-
-  final String CategoryHintText = AppStrings.categoryText;
-  final List<String> CategoryDropdownList = ['Category A', 'Category B', 'Category C', 'Category D'];
-
-  final String CharityHintText = AppStrings.charityText;
-  final List<String> CharityDropdownList = ['Charity A', 'Charity B', 'Charity C', 'Charity D'];
-
-  final String PriceHintText = AppStrings.priceText;
-  final List<String> PriceDropdownList = ["£10.0", "£20.99", "£30.99", "£15.99"];
-
-  final String ConditionHintText = AppStrings.conditionText;
-  final List<String> ConditionDropdownList = ["Condition A", "Condition B", "Condition C", "Condition D"];
 
 
 
