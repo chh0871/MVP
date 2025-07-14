03da8c6a0fd7e50ee5381001300cf23d56c71710
Mat Gallacher <mat.gallacher@gmail.com>
2025-07-14 21:42:10 +0100

    Product page cleanup

--- lib/features/home/widgets/product_card.dart
+++ lib/features/home/widgets/product_card.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:cherry_mvp/core/models/model.dart';
-import 'product_info_card.dart';
 
 class ProductCard extends StatelessWidget {
   final Product product;
@@ -82,7 +81,56 @@ class ProductCard extends StatelessWidget {
                     ),
                   ],
                 )),
-            ProductInfoCard(product: product),
+            const SizedBox(height: 8),
+            Text(
+              product.name,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 1,
+              style: Theme.of(context).textTheme.titleSmall,
+            ),
+            const SizedBox(height: 4),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Text(
+                    product.quality,
+                    overflow: TextOverflow.ellipsis,
+                    style: Theme.of(context).textTheme.labelMedium,
+                  ),
+                ),
+                Text(
+                  product.size,
+                  style: Theme.of(context).textTheme.labelMedium,
+                ),
+              ],
+            ),
+            const SizedBox(height: 4),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Text(
+                    '£${product.price.toStringAsFixed(2)}',
+                    style: Theme.of(context).textTheme.labelMedium?.copyWith(
+                          color: Theme.of(context).colorScheme.secondary,
+                        ),
+                  ),
+                ),
+                Image.asset(
+                  product.charity_image,
+                  height: 20,
+                  width: 20,
+                  fit: BoxFit.cover,
+                ),
+              ],
+            ),
+            Text(
+              '£${product.price.toStringAsFixed(2)}',
+              style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+            ),
           ],
         ),
       ),
